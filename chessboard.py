a4bbe3ff3fd70f9055f2b78dde7cc1939326c4ae
jerivas <jerivas@uaq.edu.mx>
2021-05-07 18:41:16 -0500

    Add Chessboard class

---
+++ chessboard.py
@@ -0,0 +1,14 @@
+
+class ChessBoard():
+    def __init__(self, name):
+        self.name = name
+
+    def draw(self):
+        print('Chessboard: ', self.name)
+        print("# # # # # ")
+        print("# # # # # ")
+        print("# # # # # ")
+        print("# # # # # ")
+        print("# # # # # ")
+        print("# # # # # ")
+        print("# # # # # ")
--- main.py
+++ main.py
@@ -1,2 +1,10 @@
+from chessboard import ChessBoard
+
 print('Chess game')
- 
+
+obj1 = ChessBoard('chessboard1')
+obj2 = ChessBoard('chessboard2')
+
+obj1.draw()
+
+obj2.draw()
