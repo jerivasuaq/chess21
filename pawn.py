c4b604bf9232b8c1530d6d3d4ca0f01502f6623b
aespinosa <adan.espinosa.jimenez@gmail.com>
2021-05-12 20:56:21 -0500

    Agragados movimientos basicos de peon y funcion para verificar posibilidad de comer

--- pawn.py
+++ pawn.py
@@ -2,8 +2,26 @@
 from piece import Piece
 
 
+
 class Pawn(Piece):
     def __init__(self, row, col, player=1):
         super().__init__(row, col, player)
-        self.char = 'P'
+        self.char = 'P'  
+        self.move_1 = True
+        
 
+    def move (self, drow, dcol, board):
+    	can_eat = (eat(board, dcol, drow))
+	if self.move_1 ==True and drow<3 or (dcol<2 and can_eat==True):
+		return True
+	if else self.move_1 == False and drow<2 or (dcol<2 and can_eat==True):
+		return True
+	else 
+		return False
+	self.move_1 = False
+	
+    def eat (board, dcol, drow):
+    	if (chessBoard.board[dcol+1][drow+1]!=None) or ((chessBoard.board[dcol+1][drow-1]!=None)):
+    		if (chessBoard.board[dcol+1][drow+1].player != self.player) or ((chessBoard.board[dcol+1][drow-1]!=self.player)
+    			return True
+    			
